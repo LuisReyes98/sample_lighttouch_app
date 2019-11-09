51021b69f5084e65b43f8e6ec6799f2534c5efed
Luis Reyes <luisrogelio98@gmail.com>
2019-11-09 19:09:29 -0400

    creating a new URL path

---
+++ packages/main-package/actions/post_action.lua
@@ -0,0 +1,12 @@
+event = ["view_posts"]
+priority = 2
+input_parameters = ["request"]
+
+return {
+    status = 200,
+    headers = {
+        ["content-type"] = "text/html",
+    },
+    body = render("post.html", {
+    })
+}
