c26a7f36c724b4f1df56dd01a4b6ded1e5f3abcc
Luis Reyes <luisrogelio98@gmail.com>
2019-11-09 18:46:45 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sample app
+
+basic [lighttouch](https://github.com/foundpatterns/lighttouch) app for understanding the framework.
---
+++ packages/main-package/actions/home_action.lua
@@ -0,0 +1,11 @@
+event = ["home"]
+priority = 1
+input_parameters = ["request"]
+
+return {
+    status = 200,
+    headers = {
+        ["content-type"] = "text/html",
+    },
+    body = render("index.html", {})
+}
