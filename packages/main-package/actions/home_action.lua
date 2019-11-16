6736ad3c6d9d907e1b10c20a2fc1e1601c02761a
Luis Reyes <luisrogelio98@gmail.com>
2019-11-16 02:42:43 -0400

    use of models in package

--- packages/main-package/actions/home_action.lua
+++ packages/main-package/actions/home_action.lua
@@ -2,6 +2,11 @@ event = ["home"]
 priority = 1
 input_parameters = ["request"]
 
+
+local model = models['project']
+
+log.debug(json.from_table(model))
+
 return {
     status = 200,
     headers = {
