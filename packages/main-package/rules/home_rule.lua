5ecc6ef01b7a5f8ca8702e9d6b2480edbab1c53a
Luis Reyes <luisrogelio98@gmail.com>
2019-12-20 01:01:27 -0400

    comment examples

--- packages/main-package/rules/home_rule.lua
+++ packages/main-package/rules/home_rule.lua
@@ -5,3 +5,12 @@ events_table = ["home"]
 request.method == "GET"
 and
 #request.path_segments == 0
+-- and
+-- uuid.check(request.path_segments[1]) --example uuid /dasda238120398098419024dalk
+-- or
+-- models[request.path_segments[1] -- /model_name
+-- request.headers["accept"]:match("text/html")
+-- request.path_segments[1] == "path" -- /path
+--request.query.code == '3213' --/?code="3213"
+-- or
+-- request.query.name --/?name="Any Value"
